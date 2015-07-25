1aaaef3b1e00b46dfb911f2fbf96d7dd57b7bb9b
Jens Gutermuth <jens@gutermuth-online.de>
2015-07-25 19:05:20 +0200

    Overwrite SQLALCHEMY_DATABASE_URL if it's in the environment

--- shortlink/app.py
+++ shortlink/app.py
@@ -9,7 +9,8 @@ from rq import Queue
 app = Flask('shortlink')
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgres://shortlink:shortlink@localhost/shortlink'
-for k in ['PIWIK_SITE_ID', 'PIWIK_URL', 'PIWIK_TOKEN']:
+for k in ['PIWIK_SITE_ID', 'PIWIK_URL', 'PIWIK_TOKEN',
+          'SQLALCHEMY_DATABASE_URI']:
     if k in os.environ:
         app.config[k] = os.environ[k]
 db = SQLAlchemy(app)
